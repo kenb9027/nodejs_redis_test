f9de9877861ce2b5e265c637ecfd152af48e8413
kenb9027 <kenb9027@gmail.com>
2022-11-22 11:15:12 +0100

    initial commit , ok to test redis with 2 simpes routes

---
+++ app.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const axios = require("axios");
+const redis = require("redis");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+let redisClient;
+
+(async () => {
+  redisClient = redis.createClient();
+
+  redisClient.on("error", (error) => console.error(`Error : ${error}`));
+
+    await redisClient.connect();
+    console.log('redisClient connected !')
+})();
+
+
+app.get("/", (req, res) => {
+    
+    res.send("Hello, World!");
+    
+});
+
+app.get("/redis-set/:id", (req, res) => {
+    let id = req.params.id;
+    
+    console.log('set redis')
+    redisClient.set(id, 'Date : ' + Date() , function(err, reply) {
+        // console.log(reply); // OK
+    });
+    
+    
+    res.send("key " + id + " ajouté !");
+    
+});
+
+app.get("/redis-get/:id", (req, res) => {
+    
+    let id = req.params.id;
+
+    async function getData(req, res) {
+        let results;
+        let isCached = false;
+      
+        try {
+          const cacheResults = await redisClient.get(id);
+          if (cacheResults) {
+            isCached = true;
+            results = cacheResults;
+          } else {
+            results = 'Date : ' + Date();
+            await redisClient.set(id, results)
+          }
+      
+          res.send({
+            fromCache: isCached,
+            data: "key " + id + " = " +results,
+          });
+        } catch (error) {
+          console.log(error)
+        }
+    }
+    
+    getData(req, res);
+
+
+    // console.log('get redis')
+    // redisClient.get('redis', function (err, reply) {
+    //     console.log(reply); // Bonjour !
+    //     res.send(reply);
+    // });
+
+    
+});
+
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+  });
